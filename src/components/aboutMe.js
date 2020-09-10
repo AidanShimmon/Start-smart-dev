7d0ece0d761b42ed2d08a1725847b04f2f1269ea
Aidan Shimmon <aidanshimmon@aidans-mbp.home>
2020-09-10 19:41:09 +0100

    Roughly built about me section and started projects area

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -1,15 +1,21 @@
 import React from 'react'
 
+import AboutMe from "../assets/aboutMe.svg";
+
 const aboutMe = () => {
   return (
     <section className="about">
       <div className="about__content">
         <div className="about__content__text">
-          <h3 className="about__watermark">About</h3>
-          <h3>About</h3>
-          <p>Web developer</p>
+          <span className="about__watermark">About</span>
+          <h3>About me.</h3>
+          <h4>Web developer</h4>
           <p>Specialized in delivering <strong>modern</strong>, <strong>clean</strong>, <strong>fast</strong> and <strong>effective</strong> websites. My skillset consists of HTML, SCSS, JavaScript, Wordpress and Vue. </p>
           <p>Outside of web development I am passionate about music, excersice and sustainability.</p>
+        </div>
+
+        <div className="about__content__image">
+          <AboutMe/>
         </div>
       </div>
     </section>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React from "react"
 import Layout from "../components/layout"
 import Hero from "../components/hero"
 import AboutMe from "../components/aboutMe"
+import Project from "../components/projects"
 
 const IndexPage = () => (
   <Layout>
     <Hero/>
     <AboutMe/>
+    <Project/>
   </Layout>
 )
 
