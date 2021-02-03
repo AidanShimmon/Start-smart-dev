39c6342eb630094a9ed35837af19c8f966a2d944
AidanShimmon <aidanshimmon@hotmail.co.uk>
2021-02-03 18:12:48 +0000

    styling and gsap timeline updates

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -8,42 +8,21 @@ gsap.registerPlugin(ScrollTrigger);
 export default class aboutMe extends Component {
   componentDidMount() {  
     
-    const about = document.getElementById("about__content");
+    const about = document.getElementById("about");
+    const about__watermark__text = document.getElementById("about__watermark__text");
     const about__text = document.getElementById("about__content__text");
     const about__image = document.getElementById("about__content__image");
     
     var tl = gsap.timeline({
-      scrollTrigger: about,
-      pin: true,   // pin the trigger element while active
-      start: "top top", // when the top of the trigger hits the top of the viewport
+      scrollTrigger: about__text,
+      start: "top center", // when the top of the trigger hits the top of the viewport
       end: "+=500", // end after scrolling 500px beyond the start
-      scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
     });
 
-    tl.from(about, {
-      scaleX: 0, 
-      opacity: 0.3,
-      transformOrigin:"left",
-      duration: 0.6,
-    })
-    .from(about__text, {
-      opacity: 0
-    })
-    .from(about__image, {
-      opacity: 0
-    })
-
-    // gsap.from(about__text, {
-    //   scrollTrigger: about,
-    //   delay: 0.6,
-    //   opacity: 0,
-    // })
-
-    // .from(about__image, {
-    //   scrollTrigger: about,
-    //   delay: 0.6,
-    //   opacity: 0,
-    // })
+    tl.from(about__text, {duration: 0.4, opacity: 0, y: 200 })
+    .from(about__image, {duration: 0.5,opacity: 0}, "-=0.3")
+    .from(about__watermark__text, {duration: 0.5, opacity: 0, y: 200 }, "-=0.3")
+    
   } 
 
   render() {
@@ -51,7 +30,7 @@ export default class aboutMe extends Component {
       <section className="about" id="about">
         <div className="about__content" id="about__content">
           <div className="about__content__text" id="about__content__text">
-            <span className="about__watermark">About</span>
+            <span className="about__watermark"><h4 id="about__watermark__text">About</h4></span>
             <h3>About</h3>
             <p>Specialized in delivering <strong>modern</strong>, <strong>fast</strong>, <strong>secure</strong> and <strong>effective</strong> websites.</p>
             <p>Each project is approached as a unique problem to solve. During our discovery phase we will gather information about your business so we can best develop a solution to specifically match your values, requirements and style.</p>
